2c048add971f2cf7a4542fe1945b909cca8e6ea6
Redi Ahmad <redi.ngoding@gmail.com>
2023-09-29 16:06:05 +0700

    feat: range flow slider

--- src/script.js
+++ src/script.js
@@ -2,3 +2,15 @@ function setBank(bankName) {
 	const chosenBank = document.getElementById('chosen-bank')
 	chosenBank.textContent = bankName
 }
+
+// Range Utils
+const totalFlowRangeElement = document.getElementById('total-flow')
+const currentFlowValue = document.getElementById('current-flow-value')
+
+// get current range value
+currentFlowValue.textContent = totalFlowRangeElement.value
+
+// update range value
+totalFlowRangeElement.addEventListener('input', function () {
+	currentFlowValue.textContent = totalFlowRangeElement.value
+})
